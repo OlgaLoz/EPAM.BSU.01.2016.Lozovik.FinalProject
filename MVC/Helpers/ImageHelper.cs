2af09ee4e851f6b668221d83948e4aecd120eec7
Olga Lozovik <lozovikolga@gmail.com>
2016-06-24 13:47:09 +0300

    All project.

---
+++ MVC/Helpers/ImageHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Helpers
+{
+    public static class ImageHelper
+    {
+        public static MvcHtmlString Image(this HtmlHelper html, byte[] image, string alt, string height, string width)
+        {          
+            TagBuilder tagBuilder = new TagBuilder("img");
+            tagBuilder.MergeAttribute("src",image != null 
+                ? $"data:image/jpeg;base64,{Convert.ToBase64String(image)}" 
+                : VirtualPathUtility.ToAbsolute("~/Content/Images/noimage.png"));
+            tagBuilder.MergeAttribute("alt", alt);
+            tagBuilder.MergeAttribute("height", height);
+            tagBuilder.MergeAttribute("width", width);
+
+            return MvcHtmlString.Create(tagBuilder.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
